2a0e1cfcc8720a597deb0df74d872b9575555bd5
王云阳 <511763880@qq.com>
2024-04-26 15:42:28 +0800

    fix(CORS设置): 添加线上地址

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -5,7 +5,10 @@ export const CorsMiddleware = (
     res: Response,
     next: NextFunction
 ) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header(
+        "Access-Control-Allow-Origin",
+        "http://localhost:5173,https://hosts.balder.wang/"
+    );
     res.header(
         "Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, OPTIONS"
