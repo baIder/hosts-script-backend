064df3359dd45297ce8da9ad0e0604fe0ef07932
王云阳 <511763880@qq.com>
2024-04-26 15:48:56 +0800

    fix(CORS): 修复跨域问题

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -5,10 +5,7 @@ export const CorsMiddleware = (
     res: Response,
     next: NextFunction
 ) => {
-    res.header(
-        "Access-Control-Allow-Origin",
-        "http://localhost:5173,https://hosts.balder.wang/"
-    );
+    res.header("Access-Control-Allow-Origin", "https://hosts.balder.wang/");
     res.header(
         "Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, OPTIONS"
