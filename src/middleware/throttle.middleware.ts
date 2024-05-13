ec50331338ca9e1c86a55cc1d43bf483ce84a413
王云阳 <511763880@qq.com>
2024-05-13 13:44:05 +0800

    feat(接口防刷): 相同ip在30s内只能请求一次

--- src/middleware/throttle.middleware.ts
+++ src/middleware/throttle.middleware.ts
@@ -1,11 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 import redis from "../utils/redis";
+import { logger } from "../utils/logger";
 
-export const throttleMiddleware = (
+export const throttleMiddleware = async (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
-    redis.incr("reqCount");
+    const ip = (req.headers["X-Real-IP"] as string) || req.ip || "unknown ip";
+    try {
+        const body = await redis.get(ip);
+        if (body) {
+            return res.status(429).send("请求提交过多，请30秒后再试");
+        }
+    } catch (error) {
+        logger.error(error);
+    }
+    redis.set(ip, req.body, "EX", 30);
     next();
 };
